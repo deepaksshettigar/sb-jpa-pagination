77843966ac51fdc0a216a037bd4430cf2f4136be
deepaksshettigar <deepaksshettigar@gmail.com>
2024-02-24 01:14:05 +0530

    Change to JpaRepository

--- splitrun-common/src/main/java/com/arcadia/splitrun/repo/SplitRunRepository.java
+++ splitrun-common/src/main/java/com/arcadia/splitrun/repo/SplitRunRepository.java
@@ -3,12 +3,11 @@ package com.arcadia.splitrun.repo;
 import com.arcadia.splitrun.model.SplitRun;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigInteger;
 
-public interface SplitRunRepository extends PagingAndSortingRepository<SplitRun, BigInteger>, CrudRepository<SplitRun, BigInteger> {
+public interface SplitRunRepository extends JpaRepository<SplitRun, BigInteger> {
     Page<SplitRun> findByNameIgnoreCaseContaining(String name, Pageable pageable);
 
 }
